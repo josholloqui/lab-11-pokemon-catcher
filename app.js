b5e2ca41a7f4d7b44f4a5325c4e098ad0f6a81d4
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-07-21 00:08:51 -0700

    adds eventlistener

--- app.js
+++ app.js
@@ -29,11 +29,50 @@ function setPage() {
         wildEncounter3 = getRandomPokemon(collectWildPokemon);
     }
 
-    console.log(wildEncounter1)
-    console.log(wildEncounter2)
-    console.log(wildEncounter3)
+    const pokeLabels = document.querySelectorAll('label');
+    let inputs = [];
+    let names = [];
+    let images = [];
+    for (let i = 0; i <= 2; i++) {
+        names[i] = pokeLabels[i].children[0];
+        inputs[i] = pokeLabels[i].children[1];
+        images[i] = pokeLabels[i].children[2];
+    }
+ 
+    inputs[0].value = wildEncounter1.id;
+    images[0].src = wildEncounter1.url_image;
+    names[0].textContent = wildEncounter1.pokemon;
+    inputs[0].addEventListener('click', choosePokemonEvent);
+
+    inputs[1].value = wildEncounter2.id
+    images[1].src = wildEncounter2.url_image;
+    names[1].textContent = wildEncounter2.pokemon;
+    inputs[1].addEventListener('click', choosePokemonEvent);
+
+    inputs[2].value = wildEncounter3.id
+    images[2].src = wildEncounter3.url_image;
+    names[2].textContent = wildEncounter3.pokemon;
+    inputs[2].addEventListener('click', choosePokemonEvent);
+
+    inputs[0].disabled = false;
+    inputs[1].disabled = false;
+    inputs[2].disabled = false;
 }
 
+function choosePokemonEvent(e) {
+    capturedPokemon++;
+
+    const chosenPokemon = e.target.value;
+    console.log(chosenPokemon)
+
+    // const pokeLabels = document.querySelectorAll('label');
+    // let inputs = [];
+
+    // for (let i = 0; i <= 2; i++) {
+    //     inputs[i] = pokeLabels[i].children[1];
+    // }
+
+}
 
 
 

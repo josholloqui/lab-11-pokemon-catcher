ec85221acc059f87d2af76bd4c539c687c8eba68
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-07-22 11:36:13 -0700

    add munge test

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,5 +1,5 @@
 import pokemonArray from '../data/pokemon.js';
-import { findById } from '../common/utils.js';
+import { findById, mungeCaptured, mungeEncountered, mungeNames } from '../common/utils.js';
 // import { example } from '../example.js';
 
 const test = QUnit.test;
@@ -29,3 +29,61 @@ test('should find pokemon by its id', assert => {
     assert.equal(findPokemon2.pokemon, expected2);
 });
 
+const pokeData = [
+    {
+        name: 'pikachu',
+        captured: 4,
+        encountered: 8
+    }, {
+        name: 'snorlax',
+        captured: 1,
+        encountered: 1
+    }, {
+        name: 'slowbro',
+        captured: 99,
+        encountered: 99
+    }, {
+        name: 'meowth',
+        captured: 1,
+        encountered: 11
+    }
+];
+
+test('should take in an array and return their names only', assert => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = ['pikachu', 'snorlax', 'slowbro', 'meowth'];
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = mungeNames(pokeData);
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    assert.deepEqual(expected, actual);
+});
+
+test('should take in an array and return their captured numbers only', assert => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = [4, 1, 99, 1];
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = mungeCaptured(pokeData);
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    assert.deepEqual(expected, actual);
+});
+
+test('should take in an array and return their encountered numbers only', assert => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = [8, 1, 99, 11];
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = mungeEncountered(pokeData);
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    assert.deepEqual(expected, actual);
+});
